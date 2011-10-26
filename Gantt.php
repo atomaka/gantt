bb6c7942beead0799ed8802dbb34610d2784d4d7
Andrew Tomaka <atomaka@gmail.com>
2011-10-26 11:00:42 -0400

    And readding the same file starting with an uppercase character...

---
+++ Gantt.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * gantt - File for the gantt class
+ *
+ * Takes data for events from users to create a basic gantt chart that can
+ * be displayed in various formats.
+ *
+ * @author Andrew Tomaka
+ * @version 1.0
+ */
+
+/**
+ * gantt - Gantt chart class.
+ *
+ * Manage events added by a user to produce a gantt chart
+ *
+ * @author Andrew Tomaka
+ * @version 1.0
+ **/
+class Gantt {
+	/**
+	 * A list of events added by the user.
+	 * @access private
+	 * @var array
+	 **/
+	private $events		= array();
+
+	/**
+	 * Constructor
+	 **/
+	function __construct() {
+		
+	}
+}
+?>
