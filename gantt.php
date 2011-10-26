c6920b8a8779ae7d53dca243faaf5a1531a06f77
Andrew Tomaka <atomaka@gmail.com>
2011-10-26 10:48:37 -0400

    Initial commit.

---
+++ gantt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * gantt - File for the gantt class
+ *
+ * Takes data for events from users to create a basic gantt chart that can
+ * be displayed in various formats.
+ *
+ * @author Andrew Tomaka
+ * @version 1.0
+ */
+
+/**
+ * gantt - Gantt chart class.
+ *
+ * Manage events added by a user to produce a gantt chart
+ *
+ * @author Andrew TOmaka
+ * @version 1.0
+ **/
+?>
